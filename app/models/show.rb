e315e8a4847d9bb9002ccbc9dcc9ea472a77d330
Tammarra McPhaul <shaemc22@gmail.com>
2021-03-27 18:25:55 -0400

    commit tree 9a62ea87fe9c809bfba826b9a510049da42bb9e7

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+   
+
+  def actors_list
+    list = []
+        self.characters.each do |character|
+            list << "#{character.actor.first_name} #{character.actor.last_name}"
+        end
+    list
+    end
 end
---
+++ db/migrate/008_add_character_to_shows.rb
@@ -0,0 +1,5 @@
+class AddCharacterToShows < ActiveRecord::Migration[5.1]
+	def change
+		add_column :shows, :character_id, :integer
+	end
+end
